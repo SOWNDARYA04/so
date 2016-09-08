f0f85faeba956e428cc972078f9fa1792a456e11
SOWNDARYA04 <sowndabi@gmail.com>
2016-09-08 17:54:39 +0530

    Update and rename bg1.java to bg2.java

---
+++ bg2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class bg2
+{
+public static void main(String args[])
+{
+scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+system.out.println("the number is a positive number");
+
+}
+else if(a<0)
+{
+System.out.println("negative");
+}
+else 
+{
+System.out.println("zero");
+}
+}
+}
